ef1e90535f4479014592d9bd86a074695971b108
Ahmet Unsal <66937298+AhmetUnsal98@users.noreply.github.com>
2022-12-28 08:57:13 +0300

    Adding responsive design without footer dropdowns

--- src/components/jumbotron/Jumbotron.js
+++ src/components/jumbotron/Jumbotron.js
@@ -4,6 +4,9 @@ import image1 from "../../img/image1.jpg";
 import image2 from "../../img/image2.jpg";
 import image3 from "../../img/image3.jpg";
 import getir from "../../img/getir.png";
+import { IoLocationSharp } from "react-icons/io5";
+import { IoArrowForwardOutline } from "react-icons/io5";
+
 const dummyData = [
   {
     id: "1",
@@ -48,6 +51,31 @@ const Jumbotron = () => {
 
   return (
     <div className="jumbotron">
+      <div className="jumbotron-mobile">
+        <div className="jumbotron-mobile-header">
+          <h4>getir</h4>
+        </div>
+        <div className="jumbotron-mobile-address-container">
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <IoLocationSharp size={15} color="rgb(76, 51, 152)" />{" "}
+            <p>Teslimat Adresini Belirle</p>
+          </div>
+          <IoArrowForwardOutline
+            style={{ marginRight: "6px" }}
+            size={15}
+            color="rgb(76, 51, 152)"
+          />
+        </div>
+        <div className="jumbotron-mobile-campaign">
+          <img src="https://cdn.getir.com/misc/611e55d33ea65bef40f9ba05_banner_tr_1629378026496.jpeg"></img>
+        </div>
+      </div>
       <div className="jumbotron-main-container">
         <div
           className="jumbotron-slider"
